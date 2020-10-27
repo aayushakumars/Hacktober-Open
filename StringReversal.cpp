a81aec1b92e2a3252a5c79ab7bcf478e392077ad
Aayush Kumar <53375392+aayushakumars@users.noreply.github.com>
2020-10-27 15:18:43 +0530

    Created StringReversal.cpp
    You can write any comment here, related to the file!!!!!

---
+++ StringReversal.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+void reverseWords(string str) 
+{ 
+    stack<char> st; 
+
+    for (int i = 0; i < str.length(); ++i) { 
+        if (str[i] != ' ') 
+            st.push(str[i]); 
+
+
+        else { 
+            while (st.empty() == false) { 
+                cout << st.top(); 
+                st.pop(); 
+            } 
+            cout << " "; 
+        } 
+    } 
+
+
+    while (st.empty() == false) { 
+        cout << st.top(); 
+        st.pop(); 
+    } 
+} 
+
+for example^^^^^
+
+You can write any algo!!!! (which hasnt been added already!!!!)
